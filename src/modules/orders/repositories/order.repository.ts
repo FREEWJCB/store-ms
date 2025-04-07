060a9da3c67d2ef829c301e0fba71fee3302565b
Walter Caro <desarrolloback@plass.app>
2025-04-07 13:44:29 -0500

    add repository in order

---
+++ src/modules/orders/repositories/order.repository.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Order } from '@/modules/orders/schemas/order.schema';
+import { InjectModel } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize';
+
+@Injectable()
+export class OrderRepository {
+  constructor(
+    @InjectModel(Order)
+    private readonly order: typeof Order,
+    @Inject(Sequelize) private readonly sequelize: Sequelize,
+  ) {}
+
+  public async create(attribute: Partial<Order>): Promise<Order> {
+    const transaction = await this.sequelize.transaction();
+    try {
+      const result = await this.order.create(attribute as Order, {
+        transaction,
+      });
+      await transaction.commit();
+      return result;
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  }
+}
