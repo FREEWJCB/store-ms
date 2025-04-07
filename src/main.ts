049660d3a8bca80ec7e96173a6a47c915af5e900
Walter Caro <desarrolloback@plass.app>
2025-04-06 20:59:54 -0500

    add seed in product

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { INestApplication } from '@nestjs/common';
 import { I18nValidationExceptionFilter, I18nValidationPipe } from 'nestjs-i18n';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@/app.module';
+import { ProductSeeder } from './modules/products/seeders/product.seeder';
 
 export async function getApp(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(), {
@@ -30,6 +31,11 @@ export function configureApp(app: INestApplication): INestApplication {
 
 async function bootstrap(): Promise<void> {
   const app = await getApp();
+
+  if (process.argv.includes('--seed') || process.env['SEED_DB'] === 'true') {
+    const seeder = app.get(ProductSeeder);
+    await seeder.seed();
+  }
 
   const config = new DocumentBuilder()
     .setTitle('Store microservice')
