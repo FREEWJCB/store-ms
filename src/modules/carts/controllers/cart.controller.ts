994ca5065142adba0f4663670802e904d1ee1bf1
Walter Caro <desarrolloback@plass.app>
2025-04-07 09:44:09 -0500

    add list in cart controller

--- src/modules/carts/controllers/cart.controller.ts
+++ src/modules/carts/controllers/cart.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import {
   ApiOperation,
   ApiTags,
   ApiCreatedResponse,
+  ApiOkResponse,
+  getSchemaPath,
 } from '@nestjs/swagger';
 import { Cart } from '@/modules/carts/schemas/cart.schema';
 import { CartService } from '@/modules/carts/services/cart.service';
@@ -13,7 +15,22 @@ import { CartCreateDto } from '@modules/carts/dtos/cart.create.dto';
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
+  @Get()
+  @HttpCode(200)
+  @ApiOperation({ summary: 'List of carts' })
+  @ApiOkResponse({
+    description: 'List of carts',
+    schema: {
+      type: 'array',
+      items: { $ref: getSchemaPath(Cart) },
+    },
+  })
+  public async index(): Promise<Cart[]> {
+    return this.cartService.lists();
+  }
+
   @Post()
+  @HttpCode(201)
   @ApiOperation({ summary: 'Create a cart' })
   @ApiCreatedResponse({
     description: 'Cart successfully created',
